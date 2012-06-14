64c9a766d6c717d407214a1fa4077cbd7618eac3
hikobiko <davidcinho_55@hotmail.com>
2012-06-14 01:16:34 -0500

    mi primer mensaje

---
+++ src/basededatos/Basededatos.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basededatos;
+
+/**
+ *
+ * @author advance
+ */
+public class Basededatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
